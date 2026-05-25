97215cafe0466c0b51179469846f906ee8402db8
hamidrezaramzani <hamidrezaramzani80@gmail.com>
2026-05-25 22:09:18 +0330

    feat: add keyboom origin

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -9,7 +9,11 @@ async function bootstrap() {
   app.use(cookieParser());
 
   app.enableCors({
-    origin: ['http://10.65.165.96:3000', 'http://localhost:3000'],
+    origin: [
+      'http://10.65.165.96:3000',
+      'http://localhost:3000',
+      'https://keyboom.ir/',
+    ],
     credentials: true,
   });
 
